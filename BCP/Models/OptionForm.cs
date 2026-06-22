321ae210919bd97e490cc0a08e17fc6a3b201fcb
marqquees <dom.marques@outlook.pt>
2026-06-22 11:21:17 +0100

    refactor: reorder options in OptionForm for consistency and clarity

--- BCP/Models/OptionForm.cs
+++ BCP/Models/OptionForm.cs
@@ -1,48 +1,48 @@
-﻿namespace BCP.Models
+﻿namespace BCP.Models;
+
+/// <summary>
+/// Classe estática que contém as opções para os campos do formulário de cadastro de livros.
+/// </summary>
+public static class OptionForm
 {
     /// <summary>
-    /// Classe estática que contém as opções para os campos do formulário de cadastro de livros.
+    ///     Lista de opções para o campo "Formato" do formulário de cadastro de livros.
     /// </summary>
-    public static class OptionForm
-    {
-        /// <summary>
-        /// Lista de opções para o campo "Formato" do formulário de cadastro de livros.
-        /// </summary>
-        public static readonly IReadOnlyList<string> Format = [
-            "Fotocópia", 
-            "Físico", 
-            ".pdf", 
-            ".docx", 
-            "Ebook", 
-            ".epub"
-        ];
+    public static readonly IReadOnlyList<string> Format =
+    [
+        "Ebook",
+        "Fotocópia",
+        "Físico",
+        ".pdf",
+        ".docx",
+        ".epub"
+    ];
 
-        /// <summary>
-        /// Lista de opções para o campo "Género" do formulário de cadastro de livros.
-        /// </summary>
-        public static readonly IReadOnlyList<string> Gender =
-        [
-            "Generalidades",
-            "Filosofia e Psicologia",
-            "Religião e Teologia",
-            "Ciências Sociais",
-            "Língua e Linguística",
-            "Ciências Naturais e Matemática",
-            "Tecnologia e Ciências Aplicadas",
-            "Artes, Desporto e Lazer",
-            "Literatura",
-            "História e Geografia Humana"
-        ];
-        
-        /// <summary>
-        /// Lista de opções para o campo "Proprietário" do formulário de cadastro de livros.
-        /// </summary>
-        public static readonly IReadOnlyList<string> Owner =
-        [
-            "Luís Ricardo Pereira",
-            "Maria Paula Pereira",
-            "Ana Luísa Marques",
-            "Daniel Marques"
-        ];
-    }
+    /// <summary>
+    /// Lista de opções para o campo "Género" do formulário de cadastro de livros.
+    /// </summary>
+    public static readonly IReadOnlyList<string> Gender =
+    [
+        "Artes, Desporto e Lazer",
+        "Ciências Sociais",
+        "Ciências Naturais e Matemática",
+        "Filosofia e Psicologia",
+        "Generalidades",
+        "História e Geografia Humana",
+        "Língua e Linguística",
+        "Literatura",
+        "Religião e Teologia",
+        "Tecnologia e Ciências Aplicadas"
+    ];
+
+    /// <summary>
+    /// Lista de opções para o campo "Proprietário" do formulário de cadastro de livros.
+    /// </summary>
+    public static readonly IReadOnlyList<string> Owner =
+    [
+        "Ana Luísa Marques",
+        "Daniel Marques",
+        "Luís Ricardo Pereira",
+        "Maria Paula Pereira"
+    ];
 }
