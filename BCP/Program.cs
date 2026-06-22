58b8ff514d5f5c151b116d68b564f702b1c5b9af
marqquees <dom.marques@outlook.pt>
2026-06-22 10:40:16 +0100

    refactor: simplify variable declarations in Program.cs

--- BCP/Program.cs
+++ BCP/Program.cs
@@ -3,7 +3,7 @@ using BCP.Data;
 using BCP.Services;
 using Microsoft.EntityFrameworkCore;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Adiciona serviços ao container.
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
@@ -18,17 +18,10 @@ builder.Services.AddHttpClient<IsbnLookup>();
 builder.Services.AddDbContext<BookContext>(option =>
     option.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-WebApplication app = builder.Build();
+var app = builder.Build();
 
 // Configura o pipeline HTTP.
 if (!app.Environment.IsDevelopment()) app.UseHsts();
-
-// Aplica as migrações pendentes a base de dados.
-using (IServiceScope scope = app.Services.CreateScope())
-{
-    DbContext db = scope.ServiceProvider.GetRequiredService<BookContext>();
-    db.Database.Migrate();
-}
 
 app.UseRouting();
 app.UseAntiforgery();
